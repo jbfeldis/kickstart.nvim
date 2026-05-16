161f373d0f9a587e7c985330ab2375eece9fd3b8
Jean-Baptiste Feldis <5403+jbfeldis@users.noreply.github.com>
2026-05-16 12:22:29 +0200

    Disable unused remote-plugin providers
    Silences 4 :checkhealth vim.provider warnings (node/perl/python/ruby).
    No remote plugins in use, providers can stay disabled.

--- after/plugin/options.lua
+++ after/plugin/options.lua
@@ -1,3 +1,10 @@
 vim.g.have_nerd_font = true
 vim.opt.relativenumber = true
 vim.opt.colorcolumn = '80'
+
+-- Disable unused remote-plugin providers (no Python/Ruby/Perl/Node neovim hosts installed)
+-- Silences the corresponding :checkhealth vim.provider warnings.
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
